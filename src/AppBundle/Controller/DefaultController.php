510cd80936a654336083b53152492d660f1d4e0f
yassine <baccouryassine@gmail.com>
2020-02-07 01:07:32 +0100

    ajouter la template de front office

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,12 +9,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/backOffice", name="homeptage")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('Back/layoutBack.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/", name="homepageFront")
+     */
+    public function indexFrontAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('Front/layoutFront.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
         ]);
     }
