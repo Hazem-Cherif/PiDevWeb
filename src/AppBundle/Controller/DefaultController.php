3a349b9465a855b662c963027bbdefec49819e1f
Hazem <hazem.cherif@esprit.tn>
2020-02-10 21:36:45 +0100

    Fix FOSUSER with register

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -9,20 +9,9 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends Controller
 {
     /**
-     * @Route("/backOffice", name="homeptage")
+     * @Route("/", name="homepage")
      */
     public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('Back/layoutBack.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
-    }
-
-    /**
-     * @Route("/", name="homepageFront")
-     */
-    public function indexFrontAction(Request $request)
     {
         // replace this example code with whatever you need
         return $this->render('Front/layoutFront.html.twig', [
