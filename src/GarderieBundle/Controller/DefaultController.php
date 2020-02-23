658456f4048c4016e8269b051c94eba3a66dbc53
Youssef <guermazi.youssef@esprit.tn>
2020-02-23 15:54:05 +0100

    métier youssef

--- src/GarderieBundle/Controller/DefaultController.php
+++ src/GarderieBundle/Controller/DefaultController.php
@@ -147,20 +147,78 @@ class DefaultController extends Controller
         return $this->render('GarderieBundle:Default:map.html.twig');
     }
 
-    public function getListEnfantaPayerAction(Request $request)
+    public function getListEnfantaPayerAction()
     {
         $em = $this->getDoctrine()->getManager();
+        //get garderie of connected responsable
         $garderie = $em->getRepository(Garderie::class)->getGarderieByResponsable($this->getUser()->getId());
+        // get list enfant of the garderie
         $listEnfant = $em->getRepository(Payement::class)->getListEnfant($garderie[0]->getNumGard());
-        $listEnfantnonPayer []= new enfant();
+        // parcour chaque enfant et verifier s'il est déja payé
         foreach ($listEnfant as $key => $enfant) {
-            if(count($em->getRepository(Payement::class)->verifPayement($enfant->getId() , new \DateTime('now')))>0){
+            if (count($em->getRepository(Payement::class)->verifPayement($enfant->getId(), new \DateTime('now'))) > 0) {
                 unset($listEnfant[$key]);
             }
         }
         return $this->render('GarderieBundle:Default:afficheListEnfantAPayer.html.twig',
             array('listEnfant' => $listEnfant));
 
+    }
+
+    public function getListEnfantDejaPayerAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        //get garderie of connected responsable
+        $garderie = $em->getRepository(Garderie::class)->getGarderieByResponsable($this->getUser()->getId());
+        // get list enfant of the garderie
+        $listEnfant = $em->getRepository(Payement::class)->getListEnfant($garderie[0]->getNumGard());
+        // parcour chaque enfant et verifier s'il est déja payé
+        $payements = array();
+        foreach ($listEnfant as $key => $enfant) {
+            $verif = $em->getRepository(Payement::class)->verifPayement($enfant->getId(), new \DateTime('now'));
+            if (count($verif) == 0) {
+                unset($listEnfant[$key]);
+            } else {
+                var_dump($payements);
+                $length = count($payements);
+                $payements[$length]=$verif[0];
+            }
+        }
+        return $this->render('GarderieBundle:Default:afficheListEnfantDejaPayer.html.twig',
+            array('listEnfant' => $listEnfant, 'payement' => $payements));
+
+    }
+
+    public function payerAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $enfant = $em->getRepository(enfant::class)->find($id);
+        return $this->render('GarderieBundle:Default:payer.html.twig',
+            array('enfant' => $enfant));
+
+    }
+
+    public function validerPayementAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $id = $request->request->get('id');
+        $typePayement = $request->request->get('selectType');
+        $payement = new Payement();
+        $enfant = $em->getRepository(enfant::class)->find($id);
+        $payement->setEnfant($enfant);
+        $payement->setStatus("payer");
+        $mydate = new \DateTime();
+        if ($typePayement == 'annee') {
+            $mydate->modify('+1 year');
+        } elseif ($typePayement == 'semester') {
+            $mydate->modify('+6 month');
+        } else {
+            $mydate->modify('+3 month');
+        }
+        $payement->setDate($mydate);
+        $em->persist($payement);
+        $em->flush();
+        return $this->redirectToRoute('listEnfantDejaPayer');
     }
 
 
